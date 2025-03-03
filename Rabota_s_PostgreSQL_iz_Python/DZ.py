ff1aa59eac4c65efaf8af6cb4de8e84483802d5e
Deniska06 <clik113@hotmail.com>
2025-03-04 00:09:17 +0300

    Update DZ.py

--- Rabota_s_PostgreSQL_iz_Python/DZ.py
+++ Rabota_s_PostgreSQL_iz_Python/DZ.py
@@ -1,4 +1,5 @@
 import psycopg2
+
 # Требуется хранить персональную информацию о клиентах:
 # имя,
 # фамилия,
@@ -16,13 +17,41 @@ import psycopg2
 # Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону.
 # Функции выше являются обязательными, но это не значит, что должны быть только они. При необходимости можете создавать дополнительные функции и классы.
 
-conn = psycopg2.connect(database="ttt", user="postgres", password="Qwerty12")
-cur = conn.cursor()
-cur.
+# conn = psycopg2.connect(database="ttt", user="postgres", password="Qwerty12")
+# with conn.cursor() as cur:
+    # cur.execute("""
+    #     drop table telefon;
+    #     drop table klient;
+    #     """)
+# Baza = 'database="ttt", user="postgres", password="Qwerty12"'
 
+def sozdanie_db():
+    conn = psycopg2.connect(database="ttt", user="postgres", password="Qwerty12")
+    with conn.cursor() as cur:
+        # cur.execute("""
+        # drop table telefon;
+        # drop table klient;
+        # """)
+        cur.execute("""
+            create table if not exists klient(
+                id integer primary key,
+                imya varchar(40) not null,
+                familiya varchar(40) not null,
+                pochta varchar(60) unique not null
+                )
+            """)
 
+        cur.execute("""
+            create table if not exists telefon (
+                id integer primary key,
+                klient_id integer not null,
+                tel_nomer integer,
+                foreign key (klient_id) references klient (id)
+            )
+        """)
 
+        conn.commit()
+        conn.close()
 
-
-
-conn.close()
+        
+    # return sozdanie_db()
