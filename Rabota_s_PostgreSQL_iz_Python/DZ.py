b0aba673fa408f691a82e2163a932d8458d61bf0
Deniska06 <clik113@hotmail.com>
2025-03-06 23:27:47 +0300

    Update DZ.py

--- Rabota_s_PostgreSQL_iz_Python/DZ.py
+++ Rabota_s_PostgreSQL_iz_Python/DZ.py
@@ -1,6 +1,5 @@
 import psycopg2
 
-
 # Функции выше являются обязательными, но это не значит, что должны быть только они. При необходимости можете создавать дополнительные функции и классы.
 
 # conn = psycopg2.connect(database="ttt", user="postgres", password="Qwerty12")
@@ -29,7 +28,7 @@ def sozdanie_db(conn):
                 id serial primary key,
                 klient_id integer not null,
                 tel_nomer varchar(20),
-                foreign key (klient_id) references klient (id)
+                foreign key (klient_id) references klient (id) ON DELETE CASCADE
             )
         """)
     print("Таблица успешно созданы.")
@@ -41,31 +40,46 @@ def add_klient(conn, imya, familiya, pochta):
             INSERT INTO klient (imya, familiya, pochta)
             VALUES (%s, %s, %s) RETURNING id
         """, (imya, familiya, pochta))
-
-    # klient_id = cur.fetchone()[0]  # Получаем ID вставленной записи
+    print("Клиент добавлен.")
     conn.commit()
-    # return klient_id
 
 # Функция, позволяющая добавить телефон для существующего клиента.
 def add_telefon(conn, klient_id, tel_nomer):
-        cur.execute("""
-            INSERT INTO telefon (klient_id, tel_nomer)
-            VALUES (%s, %s)
-        """, (klient_id, tel_nomer))
+    cur.execute("""
+        INSERT INTO telefon (klient_id, tel_nomer)
+        VALUES (%s, %s)
+    """, (klient_id, tel_nomer))
 
-
-
-        conn.commit()
-        print("Номер телефона добавлен.")
+    conn.commit()
+    print("Номер телефона добавлен.")
 
 # Функция, позволяющая изменить данные о клиенте.
 
-# Функция, позволяющая удалить телефон для существующего клиента.
-        
-# Функция, позволяющая удалить существующего клиента.
-        
-# Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону.
 
+
+# Функция, позволяющая удалить телефон для существующего клиента.
+def ydali_telefon(conn, klient_id, telefon):
+    cur.execute("""
+        delete from telefon
+        where klient_id = %s and tel_nomer = %s
+    """, (klient_id, telefon))
+
+
+    conn.commit()
+    print("Номер телефона удален.")
+
+# Функция, позволяющая удалить существующего клиента.
+def delete_client(conn, klient_id):
+    cur.execute("""
+        DELETE FROM klient
+        WHERE id = %s
+    """, (klient_id,))
+
+    conn.commit()
+    print("Клиент удален.")
+
+
+# Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону.
 
 
 with conn.cursor() as cur:
@@ -78,7 +92,9 @@ with conn.cursor() as cur:
     add_telefon(conn, 2, 87772225588)
     add_telefon(conn, 3, 82220003366)
     add_telefon(conn, 3, 89356724910)
-    
+
+    ydali_telefon(conn, 3, '89356724910')
+    # delete_client(conn, 2)
 
 conn.close()
-print("Клиент добален.")
+print("КОНЕЦ.")
